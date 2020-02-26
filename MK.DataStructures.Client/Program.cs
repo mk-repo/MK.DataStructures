df4e5687334923311a423fd5d820f21526e5c06f
Manjunath Krishnappa <>
2020-02-26 17:27:21 +0530

    Initial commit with node chain implementation

---
+++ MK.DataStructures.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using MK.DataStructures.Implementations;
+using System;
+
+namespace MK.DataStructures.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _nodeChain = new NodeChain();
+            var _integerNodeChain = _nodeChain.BuildIntegerNodeChain();
+
+            var _program = new Program();
+            _program.PrintNodeChain(_integerNodeChain);
+        }
+
+        void PrintNodeChain<T>(Node<T> node)
+        {
+            if (node == null) return;
+
+            Console.WriteLine($"Node Value: {node.Data}");
+            PrintNodeChain<T>(node.Next);
+        }
+    }
+}
---
+++ MK.DataStructures.Implementations/NodeChain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MK.DataStructures.Implementations
+{
+    public class NodeChain
+    {
+        public Node<int> BuildIntegerNodeChain()
+        {
+            var firstNode = new Node<int> { Data = 3 };
+            var middleNode = new Node<int> { Data = 5 };
+            var lastNode = new Node<int> { Data = 7 };
+
+            firstNode.Next = middleNode;
+            middleNode.Next = lastNode;
+
+            return firstNode;
+        }
+    }
+}
